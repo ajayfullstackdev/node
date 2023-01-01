297915858093b6a55c604d34450d63a38c3180ae
ajayjhaDev <ajaykumarjha05072000@gmail.com>
2023-01-01 09:54:22 +0530

    routing_to_end_points

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-const [, , operation, a, b] = process.argv;
+import http from "http";
+import { URL } from "url";
+import fs from "fs";
 
-let output;
+const readFile = (pathName) => {
+  return new Promise((res, rej) => {
+    fs.readFile(pathName, "utf-8", (err, data) => {
+      if (err) {
+        return rej("File doesn't exist !");
+      }
 
-switch (operation) {
-  case "add":
-    output = Number(a) + Number(b);
-    break;
+      res(data);
+    });
+  });
+};
 
-  case "sub":
-    output = Number(a) - Number(b);
-    break;
+const server = http.createServer(async (req, res) => {
+  try {
+    const instanceOfURL = new URL(req.url, "http://localhost:8000/");
+    console.log(instanceOfURL);
 
-  case "mul":
-    output = Number(a) * Number(b);
-    break;
+    if (instanceOfURL.pathname === "/users") {
+      const id = instanceOfURL.searchParams.get("id");
+      let jsonData = await readFile("./data/users.json");
 
-  default:
-    output = "Invalid entry";
-}
+      if (id !== null) {
+        jsonData = JSON.parse(jsonData).find((ele) => ele.id == id);
 
-console.log(output);
+        jsonData = jsonData ? JSON.stringify(jsonData) : null;
+      }
+
+      return res.end(jsonData);
+    }
+    res.end("Server says Hello !");
+  } catch (err) {
+    res.writeHead(500);
+    res.end("Server crashed, things not working as expected.");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to the port 8000...");
+});
+
+// 400 Bad request
+// 401 Unauthorized
+// 403 Forbidden
+// 404 Not found
+// 500 Internal server error
