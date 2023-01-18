a16f6c818e753d93ca5116d34a2303be43699b2b
ajayjhaDev <ajaykumarjha05072000@gmail.com>
2023-01-18 10:30:03 +0530

    mongoose

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 import express from "express";
-import morgan from "morgan";
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://ajay:jNeJqJtZRkE4QYBV@cluster0.2kqjkdb.mongodb.net/mongo_learn?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to Database !"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is mandatory"],
+    unique: [true, "Should be unique"], // Not seem to be working, existing bug
+  },
+
+  price: {
+    type: Number,
+    default: 0,
+  },
+
+  category: {
+    type: String,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema, "products");
 
 const app = express();
 
-// middleware
+app.use(express.json());
 
-app.use(express.static("./public")); // index (default "/")
+app.post("/product", (req, res) => {
+  const { title, price, category } = req.body;
 
-app.use(morgan("dev")); // meant for logging
-
-let numOfRequests = 0;
-
-app.use((req, res, next) => {
-  numOfRequests += 1;
-  next();
-});
-
-app.get("/noofrequests", (req, res) => {
-  res.json({
-    status: "success",
-    numOfRequests,
+  const product = new Product({
+    title,
+    price,
+    category,
   });
+
+  product
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      console.log(err, "Error");
+    });
 });
 
-app.listen(8000, () => {
-  console.log("Server running at...8k");
+app.listen(4000, () => {
+  console.log("Server running at 4k");
 });
