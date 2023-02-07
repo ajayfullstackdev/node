c589305ea324916759244b38868571eab90e6ec3
ajayjhaDev <ajaykumarjha05072000@gmail.com>
2023-02-07 16:36:18 +0530

    web_socket

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-import "./config.js";
+// import "./config.js";
 
-import express from "express";
-import mongoose from "mongoose";
-import router from "./routes/productRoutes.js";
-import globalErrorHandling from "./controllers/errorController.js";
-import cors from "cors";
+// import express from "express";
+// import mongoose from "mongoose";
+// import router from "./routes/productRoutes.js";
+// import globalErrorHandling from "./controllers/errorController.js";
+// import cors from "cors";
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
-const DBConnectionString = process.env.DB_CONNECTION_STRING;
+// const DBConnectionString = process.env.DB_CONNECTION_STRING;
 
-mongoose
-  .connect(DBConnectionString)
-  .then(() => console.log("Connected to Database !"))
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(DBConnectionString)
+//   .then(() => console.log("Connected to Database !"))
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-// app.use((req, res, next) => {
-//   console.log("Middleware 1");
+// // app.use((req, res, next) => {
+// //   console.log("Middleware 1");
 
-//   next();
+// //   next();
+// // });
+
+// // app.use((req, res, next) => {
+// //   console.log("Middleware 2");
+// // });
+
+// app.use("/api", router);
+// app.use(globalErrorHandling);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running at ${PORT}`);
 // });
-
-// app.use((req, res, next) => {
-//   console.log("Middleware 2");
-// });
-
-app.use("/api", router);
-app.use(globalErrorHandling);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running at ${PORT}`);
-});
